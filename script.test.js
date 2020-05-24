0e9c49967494ba3c1a7186583ac6d0f0e1cfba09
yobbad16 <dabboyrey@gmail.com>
2020-05-24 23:54:01 +0800

    Added testing scripts for search function and group into one

--- script.test.js
+++ script.test.js
@@ -2,12 +2,18 @@ const googleSearch = require('./script');
 
 const dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
 
-it('is a silly test', () => {
-  expect('hello').toBe('hello');
-  //   googleSearch('testtest', dbMock);
-});
+describe('GOOGLE_SEARCH', () => {
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  });
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
